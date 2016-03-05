bf92c5c98b1a5c48f48e56bd73cb8b83ae1b1f8d
Ashif22khan <ashif.22khan@gmail.com>
2016-03-05 17:23:49 +0530

    Ready to use with Icon
    usability has been enhanced a lot now a dynamic picture can be taken
    and be used to generate mosaic with the existing images from the gallery

--- Mosaic/Mosaic.swift
+++ Mosaic/Mosaic.swift
@@ -5,12 +5,25 @@
 //  Created by Ashif Khan on 10/02/16.
 
 import UIKit
+import AssetsLibrary
+import Photos
+//import PhotosUI
 
 class Mosaic:NSObject{
-    static var tileSize:Int=20
+    var tileSize:Int=20
+    
+    var fromImages:Array<UIImage> = Array<UIImage>()
+    
+    class func getInstance()->Mosaic{
+        struct Singleton{
+            static var instance = Mosaic()
+        }
+        return Singleton.instance
+    }
+    
     //static var scaleSize:CGFloat=1.0
     ////Resize image
-    class func scaleImage(image: UIImage, toSize newSize: CGSize) -> (UIImage) {
+    func scaleImage(image: UIImage, toSize newSize: CGSize) -> (UIImage) {
         let newRect = CGRectIntegral(CGRectMake(0,0, newSize.width, newSize.height))
         UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
         let context = UIGraphicsGetCurrentContext()
@@ -22,17 +35,17 @@ class Mosaic:NSObject{
         UIGraphicsEndImageContext()
         return newImage
     }
-    class func generateMosaik(fromImages:Array<UIImage>,forImage:UIImage,forScaleSize:CGFloat)->UIImage {
+    func generateMosaik(forImage:UIImage,forScaleSize:CGFloat)->UIImage {
         
         ///Double the size of original image....
-        let forImageWithScale:UIImage=Mosaic.scaleImage(forImage, toSize: CGSizeMake(forImage.size.width * forScaleSize, forImage.size.width * forScaleSize))
+        let forImageWithScale:UIImage = self.scaleImage(forImage, toSize: CGSizeMake(forImage.size.width * forScaleSize, forImage.size.width * forScaleSize))
         
         ///count the rows and columns to be generated based upon the tileSize
         let rows:Int = Int(forImageWithScale.size.width / CGFloat(tileSize))
         let cols:Int = Int(forImageWithScale.size.height / CGFloat(tileSize))
         
         ////cache for the tiles generated earlier
-        var tiles=Dictionary<Int,UIImage>()
+        //var tiles=Dictionary<Int,UIImage>()
         
         ///begin a new image context for our drwaing purposes
         ///this new image context acts as a canvas for drawing
@@ -40,28 +53,50 @@ class Mosaic:NSObject{
         for (var i=0;i<rows;i++){
             for(var j=0;j<cols;j++){
                 ///get a random number from 0 to number of supporting images available
-                let random=Int(arc4random_uniform(UInt32(fromImages.count)))
-                if tiles[random] == nil{
+                let random=Int(arc4random_uniform(UInt32(fromImages.count-1)))
+                /*if tiles[random] == nil{
                     /// if a tile with given number does not exists
                     /// create a tile for the given number from source image
                     /// and store it inside the dictionary(tiles)
-                    tiles[random]=Mosaic.scaleImage(fromImages[random], toSize: CGSizeMake(CGFloat(tileSize), CGFloat(tileSize)))
-                }
+                    tiles[random] = self.scaleImage(fromImages[random], toSize: CGSizeMake(CGFloat(tileSize), CGFloat(tileSize)))
+                }*/
                 /// Use the tile to draw in the image context
-                tiles[random]!.drawInRect(CGRectMake(CGFloat(i * tileSize), CGFloat(j * tileSize), CGFloat(tileSize), CGFloat(tileSize)))
+                fromImages[random].drawInRect(CGRectMake(CGFloat(i * tileSize), CGFloat(j * tileSize), CGFloat(tileSize), CGFloat(tileSize)))
             }
         }
         /// Tiles drawing is done, now clear the dictionary
-        tiles.removeAll()
+        //tiles.removeAll()
         /// Draw the overlay image in the context with opacity less then 1.0 to get the Mosaik effect
         forImageWithScale.drawInRect(CGRect(origin: CGPointZero, size: forImageWithScale.size),blendMode:.Normal, alpha:0.6)
         /// store the result of context, this is our final output
-        let finalImage:UIImage  = UIGraphicsGetImageFromCurrentImageContext();
+        let finalImage:UIImage  = UIGraphicsGetImageFromCurrentImageContext()
         
         /// clear the image context
         UIGraphicsEndImageContext();
         
         ///return the result
         return finalImage;
+    }
+
+    func fillSourceImage(){
+        let sortDescriptor:NSSortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
+        let fetchOptions:PHFetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors?.append(sortDescriptor)
+        
+        let results = PHAsset.fetchAssetsWithMediaType(.Image, options: fetchOptions)
+        let manager = PHImageManager.defaultManager()
+        
+        for var i = 0 ; i < results.count ; i++ {
+            let object:PHAsset = results.objectAtIndex(i) as! PHAsset
+            manager.requestImageForAsset(object,
+                targetSize: CGSize(width: 20.0, height: 20.0),
+                contentMode: .AspectFit,
+                options: nil) { (result, info) in
+                    self.fromImages.append(result!)
+            }
+        }
+    }
+    deinit{
+        self.fromImages.removeAll()
     }
 }
--- Mosaic/ViewController.swift
+++ Mosaic/ViewController.swift
@@ -6,15 +6,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var btnMosaic: UIButton!
+    @IBOutlet weak var btnCapture: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrView: UIScrollView!
     //// An array of images for creating mosaik patteren
-    var fromImages:Array<UIImage>=Array<UIImage>()
+    //var fromImages:Array<UIImage>=Array<UIImage>()
     var toggleImage:Bool=true
     var pattern:UIImage?
     var mainImage:UIImage?
-    var imageView:UIImageView?
-    override func viewDidLoad() {
+    //var imageView:UIImageView?
+    /*override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -29,11 +33,6 @@ class ViewController: UIViewController {
         btnGenerate.addTarget(self, action: "generate:", forControlEvents: .TouchUpInside)
         btnGenerate.frame = CGRectMake((self.view.frame.size.width-150)/2, self.view.frame.size.height-60, 150, 30)
         
-        
-        for(var i=1;i<121;i++){
-            fromImages.append(UIImage(named: "\(i).png")!)
-        }
-        
         //// Here is the desired pattren for which mosaic will be created
         mainImage=UIImage(named: "sachinTwo.png")
         
@@ -47,15 +46,71 @@ class ViewController: UIViewController {
         //// add the imageview as a subview to self.view
         self.view.addSubview(imageView!)
         self.view.addSubview(btnGenerate)
-        
+        //self.fromImages.removeAll()
         
     }
     func generate(sender:UIButton){
         if toggleImage {
             if pattern == nil {
-                dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)) { // 1
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { // 1
+                    for(var i=1;i<121;i++){
+                        self.fromImages.append(UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("\(i).png", ofType: "")!)!)
+                    }
                     ///generate mosaic on pattern with support images
                     self.pattern = Mosaic.generateMosaik(self.fromImages, forImage: self.mainImage!, forScaleSize: 1.0)
+                    //// get the image data and save it inside document folder
+                    //// the saved image can later be used to share
+                    let imageData = NSData(data:UIImagePNGRepresentation(self.pattern!)!)
+                    let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                    let docs: String = paths[0]
+                    let fullPath = docs + "/yourNameImg.png"
+                    let result = imageData.writeToFile(fullPath, atomically: true)
+                    print(fullPath)
+                    print(result)
+                    dispatch_async(dispatch_get_main_queue()) { // 2
+                        //// set pattern as the image for the imageview
+                        self.imageView?.image = self.pattern
+                        self.fromImages.removeAll()
+                    }
+                }
+            }else {
+                imageView?.image = pattern
+            }
+        }else{
+            imageView?.image = mainImage
+        }
+        toggleImage = !toggleImage
+    }*/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { // 1
+            Mosaic.getInstance().fillSourceImage()
+        }
+        
+        self.imageView?.userInteractionEnabled = true
+        
+        //// Here is the desired pattren for which mosaic will be created
+        mainImage=UIImage(named: "sachinTwo.png")
+        
+        ///show the basic pattern on the imageview
+        self.imageView?.image = mainImage
+        
+    }
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+    
+    @IBAction func generatePattern(sender:AnyObject){
+        if toggleImage {
+            if pattern == nil {
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { // 1
+                    
+                    //Mosaic.getInstance().fillSourceImage()
+                    
+                    ///generate mosaic on pattern with support images
+                    self.pattern = Mosaic.getInstance().generateMosaik(self.mainImage!, forScaleSize: 2.0)
                     //// get the image data and save it inside document folder
                     //// the saved image can later be used to share
                     let imageData = NSData(data:UIImagePNGRepresentation(self.pattern!)!)
@@ -78,9 +133,37 @@ class ViewController: UIViewController {
         }
         toggleImage = !toggleImage
     }
+    
+    @IBAction func capture(sender:UIButton){
+        let sheet:UIAlertController = UIAlertController(title: "Choose photo", message: "Source", preferredStyle: .ActionSheet)
+        sheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: {
+            action in
+            let pickerController:UIImagePickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .Camera
+            self.presentViewController(pickerController, animated: true, completion: nil)
+        }))
+        sheet.addAction(UIAlertAction(title: "Gallery", style: .Default, handler: {
+            action in
+            let pickerController:UIImagePickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .PhotoLibrary
+            self.presentViewController(pickerController, animated: true, completion: nil)
+        }))
+        sheet.popoverPresentationController?.sourceView = self.btnCapture
+        self.presentViewController(sheet, animated: true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        self.mainImage = image //editingInfo![UIImagePickerControllerOriginalImage] as? UIImage
+        self.pattern = nil
+        self.imageView.image = image
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        self.pattern = nil
     }
 
 
